f7ea5f5c6bce63daf93bd315594b729ef8f295ff
lucazgbrl <lcs.gabriel39@gmail.com>
2026-03-07 19:03:09 -0300

    fix index imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Login } from './Login/Login';
-import { Register } from './Register/Register';
-import { Wallet } from './Wallet/Wallet';
+import Login from './Login/Login';
+import Register from './Register/Register';
+import Wallet from './Wallet/Wallet';
 
 function App() {
   return (
